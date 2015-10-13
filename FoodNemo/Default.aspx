e6b7cf81b79fa749a8d33dffa0b0a5d665387ae3
jacklooi <jiawei527@gmail.com>
2015-10-13 23:08:19 +0800

    FoodNemo (V0.0.1)
    Initial commit

---
+++ FoodNemo/Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/header.master" AutoEventWireup="true" Inherits="_Default" Codebehind="Default.aspx.cs" %>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <label class="title"><h3>Any food in Nus</h3>
+    <h3>We get it for you</h3></label>
+    <asp:TextBox ID="descriptionText" CssClass="description" runat="server" placeholder="Enter your order e.g. Curry Chicken Rice Set @ Engin Indo Panggang" />
+    <asp:TextBox ID="locationText" CssClass="textBox left location" runat="server" placeholder="Hostel A"></asp:TextBox>
+    <asp:TextBox ID="telnoText" CssClass="textBox right telno" runat="server" placeholder="+65 12345678"></asp:TextBox>
+    <div class="clear"></div>
+    <div class="btnContainer">
+        <asp:Button ID="submitBtn" CssClass="sendBtn" runat="server" Text="Order" OnClick="submit_Click" />  
+    </div>
+    <label class="err"></label>
+    <script>
+        (function ($) {
+            var btn = $('.sendBtn');
+            var form = $('#orderForm');
+            var descTextBox = $('.description');
+            var locTextBox = $('.location');
+            var telnoTextBox = $('.telno');
+            var errLabel = $('label.err');
+            btn.on('click', function () {
+                //form.validate({onsubmit: false});
+                form.validate({
+                    rules: {
+                        <%= descriptionText.UniqueID%>: {
+                            required:true
+                        },
+                        <%= locationText.UniqueID%>: {
+                            required:true
+                        },
+                        <%= telnoText.UniqueID%>: {
+                            required:true
+                        }
+                    },
+                    messages: {
+                        <%= descriptionText.UniqueID%>: '*The pick-up location field is compulsory.<br/>',
+                        <%= locationText.UniqueID%>: '*The contact number field is compulsory.<br/>',
+                        <%= telnoText.UniqueID%>: '*The pick-up order field is compulsory.<br/>'
+                    },
+                    errorPlacement: function(err, ele){
+                        err.appendTo(errLabel);
+                    }
+                });
+            });
+        })(jQuery);
+    </script>
+</asp:Content>
+
---
+++ FoodNemo/Default.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void submit_Click(object sender, EventArgs e)
+    {
+        String desc     = descriptionText.Text;
+        String loc      = locationText.Text;
+        String telno    = telnoText.Text;
+
+        //FileInfo newFile = new FileInfo(@"C:\test.xlsx"); // local path
+        FileInfo newFile = new FileInfo(Server.MapPath(@"~/test.xlsx")); // server path
+
+        if (!newFile.Exists)
+            createNewExcel(newFile);
+
+        try
+        {
+            Debug.WriteLine("Starting ...");
+            using (ExcelPackage pck = new ExcelPackage(newFile))
+            {
+                Debug.WriteLine("1 - read workbook");
+                ExcelWorkbook wb = pck.Workbook;
+                if (wb != null)
+                {
+                    Debug.WriteLine("Work Book Exist.");
+                    if (wb.Worksheets.Count > 0)
+                    {
+                        ExcelWorksheet currWS = wb.Worksheets.First();
+                        //var rowNum = currWS.Cells.Count();
+                        int rowNum = Convert.ToInt32(currWS.Dimension.End.Row.ToString());
+                        int colNum = Convert.ToInt32(currWS.Dimension.End.Column.ToString());
+                        //currWS.Cells[rowNum, colNum].Value = "asdsadaaa";
+                        //pck.Save();
+                        currWS.Cells[rowNum + 1, 1].Value = desc;
+                        currWS.Cells[rowNum + 1, 2].Value = loc;
+                        currWS.Cells[rowNum + 1, 3].Value = telno;
+                        currWS.Cells[rowNum + 1, 4].Value = "aaaa";
+                        Boolean completed = addToLastCell(pck);
+                        if (completed)
+                            sendEmail();
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            throw;
+        }
+        finally
+        {
+            Debug.WriteLine("End . . .");
+        }
+    }
+
+    private Boolean addToLastCell(ExcelPackage pck)
+    {
+        System.Timers.Timer aTimer = new System.Timers.Timer(600000);
+        aTimer.Interval = 60000000;
+        aTimer.Stop();
+        aTimer.Start();
+        
+        pck.Save();
+
+        return true;
+    }
+
+    private void sendEmail()
+    {
+        string smtpAddress = System.Configuration.ConfigurationManager.AppSettings["SMTP"];
+        int portNumber = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["Port"]);
+        bool enableSSL = true;
+
+        string emailFrom = System.Configuration.ConfigurationManager.AppSettings["From"];
+        string password = System.Configuration.ConfigurationManager.AppSettings["Pword"];
+        string emailTo = System.Configuration.ConfigurationManager.AppSettings["To"];
+        string subject = System.Configuration.ConfigurationManager.AppSettings["Subject"];
+        string body = System.Configuration.ConfigurationManager.AppSettings["Body"];
+
+        try
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFrom);
+                mail.To.Add(emailTo);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+                // Can set to false, if you are sending pure text.
+
+                //mail.Attachments.Add(new Attachment("C:\\SomeFile.txt"));
+                //mail.Attachments.Add(new Attachment("C:\\SomeZip.zip"));
+
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Credentials = new NetworkCredential(emailFrom, password);
+                    smtp.EnableSsl = enableSSL;
+                    smtp.Send(mail);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            throw;
+        }
+    }
+
+    private void createNewExcel(FileInfo file)
+    {
+        ExcelPackage pck = new ExcelPackage(file);
+        ExcelWorkbook newWorkbook = pck.Workbook;
+        ExcelWorksheet newWorksheet = newWorkbook.Worksheets.Add("Content");
+        newWorksheet.Cells[1, 1].Value = "Description";
+        newWorksheet.Cells[1, 2].Value = "Location";
+        newWorksheet.Cells[1, 3].Value = "TelNo";
+        newWorksheet.Cells[1, 4].Value = "Created";
+        pck.Save();
+    }
+}
